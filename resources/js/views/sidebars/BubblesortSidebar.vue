c26c3f8e3023c8124eab4b090f611e2d450dab92
Kevin Mensah <kevinmensah114@gmail.com>
2022-03-04 01:11:52 +0800

    Update bubblesort & palindrome icons

--- resources/js/views/sidebars/BubblesortSidebar.vue
+++ resources/js/views/sidebars/BubblesortSidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <li  class="nav-item">
         <router-link  class="nav-link" :to="{ name: 'bubble_sort' }" >
-                    <i class="fa fa-plane"></i> Bubble Sort
+                    <i class="fa fa-sort"></i> Bubble Sort
         </router-link>
     </li>
 </template>
